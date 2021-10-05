09c9f5eecd33bf4de7d67dd752fd4dd0f8883bb0
jonatapinto <jonatastdb_jp@hotmail.com>
2021-10-05 13:32:36 -0300

    chore: update manifest and meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,10 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
-        <meta name="description" content="A simple project to save my Spends" />
+        <meta
+          name="description"
+          content="A simple project to save my Spends."
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
